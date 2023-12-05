e341c1e6c2593e6192da11cfc255458f9c087657
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2023-12-05 19:41:04 -0300

    Update scripts.js

--- javascript_completo/5_ARRAYS_E_OBJETOS/js/scripts.js
+++ javascript_completo/5_ARRAYS_E_OBJETOS/js/scripts.js
@@ -1,4 +1,5 @@
 // 1 - arrays
+
 const lista = [1, 2, 3, 4, 5];
 
 console.log(lista);
@@ -10,6 +11,7 @@ const itens = ["Jonathas", true, 2, 4.12];
 console.log(itens);
 
 // 2 - mais sobre arrays
+
 const arr = ["a", "b", "c", "d"];
 
 console.log(arr[0]);
@@ -19,6 +21,7 @@ console.log(arr[2]);
 console.log(arr[10]);
 
 // 3 - propriedades
+
 const numbers = [5, 12, 4, 22];
 
 console.log(numbers.length);
@@ -129,3 +132,175 @@ delete b.age;
 
 console.log(a);
 console.log(b);
+
+// 10 - loops em arrays
+
+const users = ["Matheus", "João", "Pedro", "Lucas"]
+
+for(let i = 0; i < users.length; i++) {
+    console.log(`Listando o usuário: ${users[i]}`)
+}
+
+// 11 - push e pop
+
+const array = ["a", "b", "c"]
+
+console.log(array)
+
+console.log(array.length)
+
+array.push("d")
+
+console.log(array)
+
+console.log(array.length)
+
+array.pop(array)
+
+const itemRemovido = array.pop()
+
+console.log(itemRemovido)
+
+console.log(array)
+
+array.push("z", "x", "y")
+
+console.log(array)
+
+// 12 - sjift e unshift
+
+const letters = ["a", "b", "c"]
+
+const letter = letters.shift()
+
+console.log(letter)
+
+console.log(letters)
+
+letters.unshift("p", "q", "r")
+
+letters.unshift("z")
+
+console.log(letters)
+
+// 13 - indexOf e lastIndexOf
+
+const myElements = ["Morango", "Maça", "Abacate", "Pêra", "Abacate"]
+
+console.log(myElements.indexOf("Maça"))
+
+console.log(myElements.indexOf("Abacate"))
+
+console.log(myElements[2]);
+
+console.log(myElements[myElements.indexOf("Abacate")]);
+
+console.log(myElements.lastIndexOf("Abacate"))
+
+console.log(myElements.indexOf("Mamão"))
+
+console.log(myElements.lastIndexOf("Mamão"))
+
+// 14 - slice
+
+const testeSlice = ["a", "b", "c", "d", "e", "f"]
+
+const subArray = testeSlice.slice(2,4)
+
+console.log(subArray)
+
+console.log(testeSlice)
+
+const subArray2 =testeSlice.slice(2, 4 + 1)
+
+console.log(subArray2)
+
+const subArray3 = testeSlice.slice(10, 20)
+
+console.log(subArray3)
+
+const subArray4 = testeSlice.slice(2)
+
+console.log(subArray4)
+
+// 15 - foreach
+
+const nums = [1, 2, 3, 4, 5]
+
+nums.forEach((numero) => {
+
+    console.log(`O número é ${numero}`);
+
+})
+
+const posts = [
+    {title: "Pimeiro post", category: "PHP"},
+    {title: "Segundo post", category: "JavaScript"},
+    {title: "Terceiro post", category: "Python"},
+];
+
+posts.forEach((post) => {
+    console.log(`Exibindo post: ${post.title}, da categoria: ${post.category}`)
+})
+
+// 16 - includes
+
+const brands = ["BMW", "VW", "Fiat"]
+
+console.log(brands.includes("Fiat"))
+
+console.log(brands.includes("AUDI"))
+
+if (brands.includes("BMW")) {
+    console.log(`Temos carros da marca BMW`)
+} else {
+    console.log(`Não temos carros da marca BMW`)
+}
+
+if (brands.includes("AUDI")) {
+    console.log(`Temos carros da marca AUDI`)
+} else {
+    console.log(`Não temos carros da marca AUDI`)
+}
+
+// 17 - reverse
+
+const reverseTest = [1, 2, 3, 4, 5]
+
+reverseTest.reverse()
+
+console.log(reverseTest)
+
+// 18 - trim
+
+const trimTest = "  testando \n  ";
+
+console.log(trimTest)
+
+console.log(trimTest.trim());
+
+console.log(trimTest.length)
+
+console.log(trimTest.trim().length);
+
+// 19 - padStart
+
+const testePadStart = "1"
+
+const newNumber = testePadStart.padStart(4, "0")
+
+console.log(testePadStart)
+
+console.log(newNumber)
+
+const testePadEnd = newNumber.padEnd(10, 0)
+
+console.log(testePadEnd)
+
+// 20 - split
+
+const frase = "O rato roeu a roupa do rei de Roma"
+
+const arrayDaFrase = frase.split(" ")
+
+console.log(arrayDaFrase)
