40cd603bdc3d6331ec48c961ce99b03a15f583e6
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2023-12-24 21:11:45 -0300

    react

---
+++ react_completo/2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import FirstComponent from './components/FirstComponent'
+
+function App() {
+
+  return (
+    <>
+    <h1>Fundamentos do React</h1>
+      <FirstComponent/>
+    </>
+  )
+}
+
+export default App
