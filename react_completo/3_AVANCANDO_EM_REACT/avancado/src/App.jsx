a84d4bd6661f4d355710a6e9a002f9d0c48b5937
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2023-12-27 16:19:15 -0300

    react

--- react_completo/3_AVANCANDO_EM_REACT/avancado/src/App.jsx
+++ react_completo/3_AVANCANDO_EM_REACT/avancado/src/App.jsx
@@ -4,17 +4,22 @@ import './App.css'
 // 2 - imagem em assets
 import night from "./assets/night.jpg"
 
+// 3 - useState 
+import Data from './components/Data'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Avançando em React</h1>
+    <div className="App" style={{ paddingBottom: "500px" }}>
+        <h1>Avançando em React</h1>
       {/* 1 - imagem em public */}
       <img src="/img.jpg" alt="Alguma imagem" />
       {/* 2 - imagem em assets */}
       <img src={night} alt="Outra imagem" />
-    </>
+      {/* 3 - useState */}
+      <Data/>
+    </div>
   )
 }
 
