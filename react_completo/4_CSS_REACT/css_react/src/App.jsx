08543212438a4a39bd0d9daea7bcdf400972e291
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2023-12-29 23:50:02 -0300

    react

---
+++ react_completo/4_CSS_REACT/css_react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        {/* 1 - css global */}
+        <h1>CSS no React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
