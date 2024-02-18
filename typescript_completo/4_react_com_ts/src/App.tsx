f544f66529fc3c29ab7e3f45c7a1023d99d7b355
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2024-02-18 16:07:32 -0300

    typescript

--- typescript_completo/4_react_com_ts/src/App.tsx
+++ typescript_completo/4_react_com_ts/src/App.tsx
@@ -1,11 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// 4 - importação de componentes
+import FirstComponent from './components/FirstComponent';
+
+// 5 - desestruturando props
+import SecondComponent from './components/SecondComponent';
+import Destructuring, { Category } from './components/Destructuring';
+
+// 6 - useState
+import State from './components/State';
+
+// 8 - type
+type textOrNull = string | null;
+
+type fixed = "isso" | "ou" | "aquilo"
 
 function App() {
+
+  // 1 - variaveis 
+  const name: string = "Matheus";
+  const age: number = 30;
+  const isWorking: boolean = true; 
+
+  // 2 - funções
+  const userGreeting = (name:string): string => {
+    return `Olá, ${name}!`
+  }
+
+  // 8 - type
+  const myText:textOrNull = "Tem algum texto aqui"
+  let mySecondText:textOrNull = null
+
+  // mySecondText = "opa";
+
+  const testandoFixed:fixed = "isso"
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <p>Está trabalhando!</p>
+      )}
+      <h3>{userGreeting(name)}</h3>
+      <FirstComponent />
+      <SecondComponent name="Segundo"/>
+      <Destructuring 
+        title="Primeiro post" 
+        content="Algum conteúdo"
+        commentQty={10}
+        tags={["ts", "js"]}
+        category={Category.TS}
+      />
+      <Destructuring 
+        title="Segundo post" 
+        content="Algum conteúdo 2"
+        commentQty={5}
+        tags={["python"]}
+        category={Category.P}
+      />
+      <State />
+      {myText &&
+        <p>Tem texto na variável</p>
+      }
+      {mySecondText &&
+        <p>Tem texto na variável</p>
+      }
     </div>
   );
 }
