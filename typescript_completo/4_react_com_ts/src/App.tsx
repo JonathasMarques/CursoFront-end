8c999018ae33fd01a75b39a2acb7a680ddb5863e
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2024-02-18 18:54:25 -0300

    typescript

--- typescript_completo/4_react_com_ts/src/App.tsx
+++ typescript_completo/4_react_com_ts/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {createContext} from 'react';
 
 // 4 - importação de componentes
 import FirstComponent from './components/FirstComponent';
@@ -10,10 +10,22 @@ import Destructuring, { Category } from './components/Destructuring';
 // 6 - useState
 import State from './components/State';
 
+// 10 - utilizando contexto
+import Context from './components/Context';
+
 // 8 - type
 type textOrNull = string | null;
 
 type fixed = "isso" | "ou" | "aquilo"
+
+// 9- context
+interface IAppContext {
+  language: string;
+  framework: string;
+  projects: number
+}
+
+export const AppContext = createContext<IAppContext | null>(null)
 
 function App() {
 
@@ -35,39 +47,49 @@ function App() {
 
   const testandoFixed:fixed = "isso"
 
+  // 9 - context 
+  const contextValue: IAppContext = {
+    language: "JavaScript",
+    framework: "Express",
+    projects: 5,
+  };
+
   return (
-    <div className="App">
-      <h1>TypeScript com React</h1>
-      <h2>Nome: {name}</h2>
-      <p>Idade: {age}</p>
-      {isWorking && (
-        <p>Está trabalhando!</p>
-      )}
-      <h3>{userGreeting(name)}</h3>
-      <FirstComponent />
-      <SecondComponent name="Segundo"/>
-      <Destructuring 
-        title="Primeiro post" 
-        content="Algum conteúdo"
-        commentQty={10}
-        tags={["ts", "js"]}
-        category={Category.TS}
-      />
-      <Destructuring 
-        title="Segundo post" 
-        content="Algum conteúdo 2"
-        commentQty={5}
-        tags={["python"]}
-        category={Category.P}
-      />
-      <State />
-      {myText &&
-        <p>Tem texto na variável</p>
-      }
-      {mySecondText &&
-        <p>Tem texto na variável</p>
-      }
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>TypeScript com React</h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isWorking && (
+          <p>Está trabalhando!</p>
+        )}
+        <h3>{userGreeting(name)}</h3>
+        <FirstComponent />
+        <SecondComponent name="Segundo"/>
+        <Destructuring 
+          title="Primeiro post" 
+          content="Algum conteúdo"
+          commentQty={10}
+          tags={["ts", "js"]}
+          category={Category.TS}
+        />
+        <Destructuring 
+          title="Segundo post" 
+          content="Algum conteúdo 2"
+          commentQty={5}
+          tags={["python"]}
+          category={Category.P}
+        />
+        <State />
+        {myText &&
+          <p>Tem texto na variável</p>
+        }
+        {mySecondText &&
+          <p>Tem texto na variável</p>
+        }
+        <Context />
+      </div>
+    </AppContext.Provider>
   );
 }
 
