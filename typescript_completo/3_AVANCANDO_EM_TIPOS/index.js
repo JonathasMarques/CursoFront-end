bb2b7462adae456ab13734123a3af64e46dcb6e7
Jonathas Marques <149964627+JonathasMarques@users.noreply.github.com>
2024-02-18 14:44:16 -0300

    typescript

--- typescript_completo/3_AVANCANDO_EM_TIPOS/index.js
+++ typescript_completo/3_AVANCANDO_EM_TIPOS/index.js
@@ -45,3 +45,84 @@ const objCoord = { x: 329, y: 34.65 };
 passCoordinates(objCoord);
 // passCoordinates({nome: 1, sobrenome: 2})
 const pessoaObj = { nome: "Matheus", surname: "Battisti" };
+// 8 - props opcionais
+function showNumbers(a, b, c) {
+    console.log("A: " + a);
+    console.log("B: " + b);
+    if (c) {
+        console.log("C: " + c);
+    }
+}
+showNumbers(1, 2, 3);
+showNumbers(4, 5);
+// showNumbers(6)
+// 9 - validando argumento opcional 
+function advancedGreeting(firstName, lastName) {
+    if (lastName !== undefined) {
+        return `Olá ${firstName} ${lastName}, tudo bem?`;
+    }
+    return `Olá ${firstName}, tudo bem?`;
+}
+console.log(advancedGreeting("Jhon", "Marques"));
+console.log(advancedGreeting("João"));
+// 10 - union type
+function showBalance(balance) {
+    console.log(`O saldo da conta é R$${balance}`);
+}
+showBalance(100);
+showBalance("500");
+// showBalance(true)
+const arr2 = [1, "teste", true];
+// 11 - avancando em union types
+function showUserRole(role) {
+    if (typeof role === "boolean") {
+        return "Usuário não aprovado!";
+    }
+    return `A função do usuário é: ${role}`;
+}
+console.log(showUserRole(false));
+console.log(showUserRole("Admin"));
+function showId(id) {
+    console.log(`O ID é: ${id}`);
+}
+showId(1);
+showId("200");
+showId(123);
+function showCoords(obj) {
+    console.log(`X: ${obj.x} Y: ${obj.y} Z: ${obj.z}`);
+}
+const coordObj = {
+    x: 10,
+    y: 15,
+    z: 20
+};
+showCoords(coordObj);
+const somePerson = { name: "Jhon", age: 30 };
+console.log(somePerson);
+// type personType = {
+//	   age: number
+// }
+// 15 - literal types
+let test;
+test = "testando";
+console.log(test);
+function showDirection(direction) {
+    console.log(`A direção é ${direction}`);
+}
+showDirection("left");
+// showDirection("top")
+// 16 - non-null assertion operators
+const p = document.getElementById("some-p");
+console.log(p.innerText);
+// 17 - bigint
+let n;
+// n = 1
+n = 1000n;
+console.log(n);
+console.log(typeof n);
+console.log(n + 100n);
+// 18 - symbol
+let symbolA = Symbol("a");
+let symbolB = Symbol("a");
+console.log(symbolA == symbolB);
+console.log(symbolA === symbolB);
--- typescript_completo/3_AVANCANDO_EM_TIPOS/index.ts
+++ typescript_completo/3_AVANCANDO_EM_TIPOS/index.ts
@@ -74,3 +74,143 @@ passCoordinates(objCoord)
 // passCoordinates({nome: 1, sobrenome: 2})
 
 const pessoaObj:{nome: string, surname: string} = {nome: "Matheus", surname: "Battisti"}
+
+// 8 - props opcionais
+function showNumbers(a: number, b: number, c?: number) {
+	console.log("A: " + a)
+	console.log("B: " + b)
+	if(c) {
+		console.log("C: " + c)
+	}
+}
+
+showNumbers(1, 2, 3)
+showNumbers(4, 5)
+// showNumbers(6)
+
+// 9 - validando argumento opcional 
+function advancedGreeting(firstName: string, lastName?: string) {
+
+	if(lastName !== undefined) {
+		return `Olá ${firstName} ${lastName}, tudo bem?`
+	}
+
+	return `Olá ${firstName}, tudo bem?`
+}
+
+console.log(advancedGreeting("Jhon", "Marques"))
+console.log(advancedGreeting("João"))
+
+// 10 - union type
+function showBalance(balance: string | number) {
+	console.log(`O saldo da conta é R$${balance}`)
+}
+
+showBalance(100)
+showBalance("500")
+// showBalance(true)
+
+const arr2: Array<number | string | boolean> = [ 1, "teste", true ]
+
+// 11 - avancando em union types
+function showUserRole(role: boolean | string) {
+	if(typeof role === "boolean") {
+		return "Usuário não aprovado!"
+	}
+
+	return `A função do usuário é: ${role}`
+}
+
+console.log(showUserRole(false))
+console.log(showUserRole("Admin"))
+
+// 12 - type alias
+type ID = string | number
+
+function showId(id: ID) {
+	console.log(`O ID é: ${id}`)
+}
+
+showId(1)
+showId("200")
+showId(123)
+
+// 13 - interface
+interface Point {
+	x: number
+	y: number
+	z: number
+} 
+
+function showCoords(obj: Point) {
+	console.log(`X: ${obj.x} Y: ${obj.y} Z: ${obj.z}`)
+}
+
+const coordObj:Point = {
+	x: 10,
+	y: 15,
+	z: 20
+}
+
+showCoords(coordObj)
+
+// 14 - interface x type alias
+interface Person {
+	name: string
+}
+
+interface Person {
+	age: number
+}
+
+const somePerson: Person = {name:"Jhon", age:30}
+
+console.log(somePerson)
+
+type personType = {
+	name: string
+}
+
+// type personType = {
+//	   age: number
+// }
+
+// 15 - literal types
+let test: "testando"
+
+test = "testando"
+
+console.log(test)
+
+function showDirection(direction: "left" | "right" | "center") {
+	console.log(`A direção é ${direction}`)
+}
+
+showDirection("left")
+
+// showDirection("top")
+
+// 16 - non-null assertion operators
+const p = document.getElementById("some-p")
+
+console.log(p!.innerText)
+
+// 17 - bigint
+let n: bigint
+
+// n = 1
+
+n = 1000n
+
+console.log(n)
+
+console.log(typeof n)
+
+console.log(n + 100n)
+
+// 18 - symbol
+let symbolA:symbol = Symbol("a")
+let symbolB = Symbol("a")
+
+console.log(symbolA == symbolB)
+console.log(symbolA === symbolB)
